ae46559f7d6e91b05b6f51acca87c5c3888cf050
Erdem Kaya <kerdem@live.com>
2025-05-03 12:01:08 +0200

    Added SignIn page and Identity verification processes. User authorization processes have been done

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -36,22 +36,42 @@ builder.Services.ConfigureApplicationCookie(x =>
     x.Cookie.SecurePolicy = CookieSecurePolicy.Always;
 });
 
-
-
-
-
-
-
-
-
 var app = builder.Build();
 
 
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roleNames = { "Admin", "User" };
+
+    foreach (var roleName in roleNames)
+    {
+        var exists = await roleManager.RoleExistsAsync(roleName);
+        if (!exists)
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUserEntity>>();
+    var user = new ApplicationUserEntity { UserName = "admin@de-kaya.com", Email = "admin@de-kaya.com" };
+
+    var userExists = await userManager.Users.AnyAsync(x => x.Email == user.Email);
+    if (!userExists)
+    {
+        var result = await userManager.CreateAsync(user, "Dekaya2025!");
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, "Admin");
+        }
+
+    }
+}
 
 app.MapStaticAssets();
 
